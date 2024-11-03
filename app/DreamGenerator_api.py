0a945be7b821c04dadcb2f4df83ba7c0b0277417
jwu758 <jwu758@gatech.edu>
2024-11-03 13:46:00 -0800

    modify the api to fix some typo, and also use mangum to get the handler

--- app/DreamGenerator_api.py
+++ app/DreamGenerator_api.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from typing import Optional
 from DreamGenerator import dream_enriched_story_generator, dream_picture_generator
+from mangum import Mangum
 
 app = FastAPI()
+handler = Mangum(app)
 class StoryState:
     def __init__(self):
         self.enriched_story: Optional[str] = None
@@ -10,9 +12,11 @@ class StoryState:
 app.state.story_manager = StoryState()
 
 
-@app.get("/generate_story/{story}")
+@app.get("/generate_story")
 async def generate_story_api(story:str):
+    print(f"Received story: {story}")
     enriched_story = dream_enriched_story_generator(story)
+    print(f"enriched_story: {enriched_story}")
     app.state.story_manager.enriched_story = enriched_story
     return {"enriched_story" : enriched_story, "picture_url": None}
 
@@ -22,7 +26,7 @@ async def generate_picture_api():
     picture_url = dream_picture_generator(app.state.story_manager.enriched_story)
     return {"enriched_story" : None, "picture_url": picture_url}
 
-@app.get("/generate_story_and_picture/{story}")
+@app.get("/generate_story_and_picture")
 async def generate_story_and_picture_api(story:str):
     enriched_story = dream_enriched_story_generator(story)
     picture_url = dream_picture_generator(enriched_story)
