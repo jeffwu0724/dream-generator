2f7d714d4a6ad5b4c52da094dc99d338a16e11c5
jwu758 <jwu758@gatech.edu>
2024-11-14 00:00:52 -0800

    make the loading smooth

--- dream_generator_site/components/form.tsx
+++ dream_generator_site/components/form.tsx
@@ -1,9 +1,12 @@
+"use client";
+import React from "react";
 interface FromsProps {
   setStory: any;
   onSubmit: any;
 }
 
 const Forms: React.FC<FromsProps> = (props) => {
+  const [isPressed, setIsPressed] = React.useState(false);
   return (
     <>
       <p>Tell me your dream briefly, we will generate a detailed one</p>
@@ -14,12 +17,20 @@ const Forms: React.FC<FromsProps> = (props) => {
     placeholder="A labrador is eating spaghetti"
     onChange={(e) => props.setStory(e.currentTarget.value)}
   />
+  
   <button
-    className="bg-gradient-to-r from-blue-700 to-blue-500 hover:from-blue-600 hover:to-blue-400 disabled:opacity-50 w-full p-3 rounded-md text-lg text-white font-semibold shadow-md transition ease-in-out duration-200"
-    onClick={props.onSubmit}
-  >
-    Generate
-  </button>
+  className={`bg-gradient-to-r ${
+    isPressed ? 'from-gray-500 to-gray-300' : 'from-blue-700 to-blue-500'
+  } disabled:opacity-50 w-full p-3 rounded-md text-lg text-white font-semibold shadow-md transition ease-in-out duration-200`}
+  onClick={() => {
+    setIsPressed(true);
+    props.onSubmit();
+  }}
+  onMouseUp={() => setIsPressed(false)}
+>
+  {isPressed ? 'Generating...' : 'Generate'}
+</button>
+
 </div>
 
     </>
